68fde838fdb7bc61868e869a79bbeac7fa5c9771
panmengquan <1253177019@qq.com>
2018-10-10 11:27:20 +0800

    Initial commit

---
+++ app/src/main/java/com/project/my/gpiotest/MainActivity.java
@@ -0,0 +1,57 @@
+package com.project.my.gpiotest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.FileOutputStream;
+
+import static android.system.Os.open;
+import static android.system.OsConstants.O_RDWR;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnLight;
+    private Button btnPutout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+        btnLight = findViewById(R.id.button1);
+        btnPutout = findViewById(R.id.button2);
+        btnLight.setOnClickListener(this);
+        btnPutout.setOnClickListener(this);
+    }
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button1:
+                try{
+                    FileOutputStream fops =new FileOutputStream("/sys/class/leds/NET_MODE/brightness");
+                    fops.write("1".getBytes());
+                    fops.flush();
+                    fops.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+                break;
+            case R.id.button2:
+                try{
+                    FileOutputStream fops =new FileOutputStream("/sys/class/leds/NET_MODE/brightness");
+                    fops.write("0".getBytes());
+                    fops.flush();
+                    fops.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+}
